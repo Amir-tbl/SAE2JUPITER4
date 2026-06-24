d01cf3612cd2ee22e03920287bd7c75a0bea6172
Anas LACENE-NECER <anasln95400@yahoo.com>
2026-06-24 18:13:34 +0000

    chore(seeders): ajoute l appel explicite a DatabaseSeeder dans LocalTestSeeder pour garantir l ordre d initialisation des donnees

--- database/seeders/LocalTestSeeder.php
+++ database/seeders/LocalTestSeeder.php
@@ -24,14 +24,14 @@ class LocalTestSeeder extends Seeder
      */
     public function run(): void
     {
+        // S assurer que les roles, permissions et comptes de base existent
+        $this->call(DatabaseSeeder::class);
+
         $nbSuppliers = 10;
         $nbUsers = 20;
         $nbOrders = 100;
 
-        // Donner un mot de passe à tous les utilisateurs existants (pour login local)
-        User::whereNull('password')->orWhere('password', '')->update(['password' => bcrypt('password')]);
-
-        // Récupérer les rôles par défaut (crées dans DatabaseSeeder)
+        // Récupérer les rôles par défaut (crées par DatabaseSeeder ci-dessus)
         $roles = Role::all();
 
         // Génération de fournisseurs aléatoires
